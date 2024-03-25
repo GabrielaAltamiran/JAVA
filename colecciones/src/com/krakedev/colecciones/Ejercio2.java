05c24b02741b809a95ea34edbb6f1a71fd996bbe
Gabriela Altamirano <gabrielanieto289@gmail.com>
2024-03-24 20:01:04 -0500

    Ejercicio2

---
+++ colecciones/src/com/krakedev/colecciones/Ejercio2.java
@@ -0,0 +1,23 @@
+package com.krakedev.colecciones;
+
+import java.util.ArrayList;
+
+public class Ejercio2 {
+
+	public static void main(String[] args) {
+		ArrayList<Persona> personas;
+		personas = new ArrayList<>();
+
+		personas.add(new Persona("Kevin","Teran"));
+		personas.add(new Persona("Gaby","Altamirano"));
+		personas.add(new Persona("Wendy","Tubay"));
+		
+		System.out.println(personas.size());
+		for(int i=0;i<personas.size();i++) {
+			
+		}
+		
+		
+	}
+
+}
