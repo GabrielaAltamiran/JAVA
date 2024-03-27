d14aa25b5635ea62b9273e3bfb6304884dd9dcfa
Gabriela Altamirano <gabrielanieto289@gmail.com>
2024-03-27 18:20:48 -0500

    Reto 22

--- GABRIELA_ALTAMIRANO_Amortizacion/src/com/cmc/evaluacion/entidades/Utilitario.java
+++
@@ -1,8 +0,0 @@
-package com.cmc.evaluacion.entidades;
-
-public class Utilitario {
-	public void redondear(double valor) {
-		double numroDecimal=0.00;
-		Math.round((numroDecimal*100) /100d);
-	}
-}
